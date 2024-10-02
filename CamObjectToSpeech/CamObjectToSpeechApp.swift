66f35218629695f77878d291f69bd24147aace43
Khoirunnisa' Rizky Noor Fatimah <49296015+khoirunnisarizky@users.noreply.github.com>
2024-10-02 11:44:49 +0700

    Initial Commit

---
+++ CamObjectToSpeech/CamObjectToSpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamObjectToSpeechApp.swift
+//  CamObjectToSpeech
+//
+//  Created by khoirunnisa' rizky noor fatimah on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CamObjectToSpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
